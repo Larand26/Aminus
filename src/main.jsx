a996fce2dc562138583ad998d5e833335e684293
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-06-02 10:02:47 -0300

    style: importa o fontAwesome

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,9 @@ import "primeicons/primeicons.css"; // icons
 import "primeflex/themes/primeone-dark.css"; // dark theme
 import "primeflex/themes/primeone-light.css"; // dark theme
 
+// Import do font awesome
+import "@fortawesome/fontawesome-free/css/all.min.css"; // Font Awesome
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
