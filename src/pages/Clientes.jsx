879a9f54df39bc5fb5513a479d3e72c7658593f0
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-06-02 09:17:29 -0300

    style: cria o content de clientes

--- src/pages/Clientes.jsx
+++ src/pages/Clientes.jsx
@@ -1,7 +1,12 @@
 import BarraLateral from "../components/BarraLateral";
+import Content from "../components/Content";
 import { InputText } from "primereact/inputtext";
 import { FloatLabel } from "primereact/floatlabel";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { useState } from "react";
 const Clientes = () => {
+  const [clientes, setClientes] = useState([]);
   return (
     <div className="flex">
       <BarraLateral>
@@ -26,6 +31,15 @@ const Clientes = () => {
           <label htmlFor="inputEmail">Email</label>
         </FloatLabel>
       </BarraLateral>
+      <Content>
+        <DataTable value={clientes} paginator rows={10}>
+          <Column field="nome" header="Nome" />
+          <Column field="cnpj" header="CNPJ" />
+          <Column field="id" header="ID" />
+          <Column field="celular" header="Celular" />
+          <Column field="email" header="Email" />
+        </DataTable>
+      </Content>
     </div>
   );
 };
