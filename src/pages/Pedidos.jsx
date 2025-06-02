818ea61f39b4813342075a6549e03388962345f1
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-06-02 10:25:12 -0300

    style: cria a barra lateral da página de clientes

--- src/pages/Pedidos.jsx
+++ src/pages/Pedidos.jsx
@@ -1,11 +1,46 @@
+import BarraLateral from "../components/BarraLateral";
+import Content from "../components/Content";
+import { InputText } from "primereact/inputtext";
+import { FloatLabel } from "primereact/floatlabel";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { useState } from "react";
+import { Calendar } from "primereact/calendar";
+import { Dropdown } from "primereact/dropdown";
+import vendedoresJson from "../assets/json/vendedores.json";
 const Pedidos = () => {
   return (
-    <div>
-      <h1>Página Pedidos</h1>
-      <p>Esta é a página de informações sobre os Pedidos.</p>
-      <p>
-        O Aminus é um projeto de exemplo para demonstrar o uso do React Router.
-      </p>
+    <div className="flex">
+      <BarraLateral>
+        <FloatLabel>
+          <InputText id="inputNumero" />
+          <label htmlFor="inputNumero">Número do Pedido</label>
+        </FloatLabel>
+        <FloatLabel>
+          <InputText id="inputCnpj" />
+          <label htmlFor="inputCnpj">CNPJ</label>
+        </FloatLabel>
+        <FloatLabel>
+          <Calendar id="inputDataInicial" />
+          <label htmlFor="inputDataInicial">Data Inicial</label>
+        </FloatLabel>
+        <FloatLabel>
+          <Calendar id="inputDataFinal" />
+          <label htmlFor="inputDataFinal">Data Final</label>
+        </FloatLabel>
+        <FloatLabel>
+          <InputText id="inputSituacao" />
+          <label htmlFor="inputSituacao">Situação</label>
+        </FloatLabel>
+        <FloatLabel>
+          <Dropdown
+            options={vendedoresJson}
+            id="inputVendedor"
+            className="md:w-12rem "
+          />
+          <label htmlFor="inputVendedor">Vendedor</label>
+        </FloatLabel>
+      </BarraLateral>
     </div>
   );
 };
