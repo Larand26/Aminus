eb144ead25cfa6f10a65f5438ad2bf4a5e31dbb7
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-06-02 12:03:20 -0300

    style: cria o content na página de reservas

--- src/pages/Reservas.jsx
+++ src/pages/Reservas.jsx
@@ -1,8 +1,13 @@
 import BarraLateral from "../components/BarraLateral";
+import Content from "../components/Content";
 import { InputText } from "primereact/inputtext";
 import { FloatLabel } from "primereact/floatlabel";
+import { useState } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 
 const Reserva = () => {
+  const [reservas, setReservas] = useState([]);
   return (
     <div className="flex">
       <BarraLateral>
@@ -23,6 +28,21 @@ const Reserva = () => {
           <label htmlFor="inputNumeroPedido">Número do Pedido</label>
         </FloatLabel>
       </BarraLateral>
+      <Content>
+        <DataTable
+          value={reservas}
+          className="w-full"
+          emptyMessage="Nenhuma reserva encontrada."
+        >
+          <Column field="codigoInterno" header="Código Interno" />
+          <Column field="referencia" header="Referência" />
+          <Column field="numeroPedido" header="Número do Pedido" />
+          <Column field="nome" header="Nome" />
+          <Column field="vendedor" header="Vendedor" />
+          <Column field="quantidade" header="Quantidade" />
+          <Column field="dataReserva" header="Data da Reserva" />
+        </DataTable>
+      </Content>
     </div>
   );
 };
