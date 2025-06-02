d02af8ae159e3b91089acb76c91347048611a023
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-06-02 09:02:53 -0300

    style: cria a barra lateral na página de clientes

--- src/pages/Clientes.jsx
+++ src/pages/Clientes.jsx
@@ -1,7 +1,31 @@
+import BarraLateral from "../components/BarraLateral";
+import { InputText } from "primereact/inputtext";
+import { FloatLabel } from "primereact/floatlabel";
 const Clientes = () => {
   return (
-    <div>
-      <h1>Clientes</h1>
+    <div className="flex">
+      <BarraLateral>
+        <FloatLabel>
+          <InputText id="inputNome" />
+          <label htmlFor="inputNome">Nome</label>
+        </FloatLabel>
+        <FloatLabel>
+          <InputText id="inputCnpj" />
+          <label htmlFor="inputCnpj">CNPJ</label>
+        </FloatLabel>
+        <FloatLabel>
+          <InputText id="inputId" />
+          <label htmlFor="inputId">ID</label>
+        </FloatLabel>
+        <FloatLabel>
+          <InputText id="inputCelular" />
+          <label htmlFor="inputCelular">Celular</label>
+        </FloatLabel>
+        <FloatLabel>
+          <InputText id="inputEmail" />
+          <label htmlFor="inputEmail">Email</label>
+        </FloatLabel>
+      </BarraLateral>
     </div>
   );
 };
