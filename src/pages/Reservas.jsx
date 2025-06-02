9f288e3acde3e609d73f54aa7a643b572e09111b
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-06-02 11:53:59 -0300

    style: cria a barra lateral de reservas

--- src/pages/Reservas.jsx
+++ src/pages/Reservas.jsx
@@ -1,7 +1,28 @@
+import BarraLateral from "../components/BarraLateral";
+import { InputText } from "primereact/inputtext";
+import { FloatLabel } from "primereact/floatlabel";
+
 const Reserva = () => {
   return (
-    <div>
-      <h1>Reservas</h1>
+    <div className="flex">
+      <BarraLateral>
+        <FloatLabel>
+          <InputText id="inputReferencia" />
+          <label htmlFor="inputReferencia">Referência</label>
+        </FloatLabel>
+        <FloatLabel>
+          <InputText id="inputCodigoInterno" />
+          <label htmlFor="inputCodigoInterno">Código Interno</label>
+        </FloatLabel>
+        <FloatLabel>
+          <InputText id="inputNome" />
+          <label htmlFor="inputNome">Nome</label>
+        </FloatLabel>
+        <FloatLabel>
+          <InputText id="inputNumeroPedido" />
+          <label htmlFor="inputNumeroPedido">Número do Pedido</label>
+        </FloatLabel>
+      </BarraLateral>
     </div>
   );
 };
