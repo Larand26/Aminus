c89e391c9937e8915ef6fb376d3cbe51e887cd80
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-06-04 11:19:09 -0300

    feat: pega o código de fabricante

--- db/cubagem.js
+++ db/cubagem.js
@@ -5,9 +5,17 @@ const makeCubagem = async (arg) => {
     const codigos = arg.map((item) => item.ID_CODPRODUTO).join(",");
     const query = `SELECT [PROD_COMPRIMENTO], [PROD_LARGURA], [PROD_ALTURA] FROM [PRODUTOS] WHERE ID_CODPRODUTO IN (${codigos})`;
     const result = await connection.request().query(query);
+    const resultvwItem = await connection
+      .request()
+      .query(
+        `SELECT [ID_CODPRODUTO], [PROD_CODFABRIC] FROM [VW_ITEM] WHERE ID_CODPRODUTO IN (${codigos})`
+      );
     //junta o arg com o resultado da consulta
     const produtosComCubagem = arg.map((item) => {
       const produtoCubagem = result.recordset.find(
+        (prod) => prod.ID_CODPRODUTO === item.ID_CODPRODUTO
+      );
+      const produtoFabricante = resultvwItem.recordset.find(
         (prod) => prod.ID_CODPRODUTO === item.ID_CODPRODUTO
       );
       if (produtoCubagem) {
@@ -16,13 +24,19 @@ const makeCubagem = async (arg) => {
           PROD_COMPRIMENTO: produtoCubagem.PROD_COMPRIMENTO,
           PROD_LARGURA: produtoCubagem.PROD_LARGURA,
           PROD_ALTURA: produtoCubagem.PROD_ALTURA,
+          PROD_CODFABRIC: produtoFabricante
+            ? produtoFabricante.PROD_CODFABRIC
+            : null,
         };
       }
       return item;
     });
 
+    connection.close();
+
     return produtosComCubagem;
   } catch (error) {
+    connection.close();
     console.error("Erro ao calcular cubagem:", error);
     throw new Error(error.message || "Erro desconhecido");
   }
