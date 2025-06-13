d4a4f4c93dfa86cc23f5ef042d1ae89288024357
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-06-13 11:23:48 -0300

    style: muda o estilo da home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,53 +3,71 @@ import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate();
+  const styleLinks = "text-center w-4 mb-2";
   return (
     <div className="home w-full h-screen flex align-items-center justify-content-center">
       <div className="p-4 flex flex-column align-items-center justify-content-around bg-primary w-11 max-w-30rem h-6 max-h-20rem ">
-        <div></div>
         <div className="w-10 flex flex-wrap align-items-center justify-content-around">
-          <Button
-            icon="pi pi-box"
-            rounded
-            severity="info"
-            aria-label="Box"
-            onClick={() => navigate("/produtos")}
-          />
-          <Button
-            icon="pi pi-book"
-            rounded
-            severity="info"
-            aria-label="Book"
-            onClick={() => navigate("/notas")}
-          />
-          <Button
-            icon="pi pi-user"
-            rounded
-            severity="info"
-            aria-label="User"
-            onClick={() => navigate("/clientes")}
-          />
-          <Button
-            icon="fa fa-cart-shopping"
-            rounded
-            severity="info"
-            aria-label="List"
-            onClick={() => navigate("/pedidos")}
-          />
-          <Button
-            icon="fa fa-box-archive"
-            rounded
-            severity="info"
-            aria-label="List"
-            onClick={() => navigate("/reservas")}
-          />
-          <Button
-            icon="fa fa-camera"
-            rounded
-            severity="info"
-            aria-label="List"
-            onClick={() => navigate("/fotos")}
-          />
+          <div className={styleLinks}>
+            <Button
+              icon="pi pi-box"
+              rounded
+              severity="info"
+              aria-label="Box"
+              onClick={() => navigate("/produtos")}
+            />
+            <p className="bebas">Produtos</p>
+          </div>
+          <div className={styleLinks}>
+            <Button
+              icon="pi pi-book"
+              rounded
+              severity="info"
+              aria-label="Book"
+              onClick={() => navigate("/notas")}
+            />
+            <p className="bebas">Notas</p>
+          </div>
+          <div className={styleLinks}>
+            <Button
+              icon="pi pi-user"
+              rounded
+              severity="info"
+              aria-label="User"
+              onClick={() => navigate("/clientes")}
+            />
+            <p className="bebas">Clientes</p>
+          </div>
+          <div className={styleLinks}>
+            <Button
+              icon="fa fa-cart-shopping"
+              rounded
+              severity="info"
+              aria-label="List"
+              onClick={() => navigate("/pedidos")}
+            />
+            <p className="bebas">Pedidos</p>
+          </div>
+          <div className={styleLinks}>
+            <Button
+              icon="fa fa-box-archive"
+              rounded
+              severity="info"
+              aria-label="List"
+              onClick={() => navigate("/reservas")}
+            />
+            <p className="bebas">Reservas</p>
+          </div>
+          <div className={styleLinks}>
+            <Button
+              icon="fa fa-camera"
+              rounded
+              severity="info"
+              aria-label="List"
+              onClick={() => navigate("/fotos")}
+            />
+            <p className="bebas">Fotos</p>
+          </div>
         </div>
       </div>
     </div>
