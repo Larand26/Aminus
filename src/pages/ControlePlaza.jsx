03742f4e2148b5a42929497ffaa3c2c3a4f2aeab
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-06-17 14:40:59 -0300

    style: cria a forma de um botão de controle de plaza

--- src/pages/ControlePlaza.jsx
+++ src/pages/ControlePlaza.jsx
@@ -1,7 +1,25 @@
+import { DataTable } from "primereact/datatable";
+import Content from "../components/Content";
+import { Column } from "primereact/column";
+
 const ControlePlaza = () => {
   return (
-    <div>
-      <h1>Controle Plaza</h1>
+    <div className="flex">
+      <Content titulo={"Controle Plaza"}>
+        <DataTable
+          value={[
+            { id: 1, name: "Plaza A", status: "Active" },
+            { id: 2, name: "Plaza B", status: "Inactive" },
+            { id: 3, name: "Plaza C", status: "Active" },
+          ]}
+          scrollable
+          scrollHeight="400px"
+        >
+          <Column sortable field="id" header="ID" />
+          <Column sortable field="name" header="Name" />
+          <Column sortable field="status" header="Status" />
+        </DataTable>
+      </Content>
     </div>
   );
 };
