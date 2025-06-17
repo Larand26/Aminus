666b986661b9037352e367486deef9c7f05d8bbd
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-06-17 15:06:37 -0300

    style: coloca as colunas na plaza

--- src/pages/ControlePlaza.jsx
+++ src/pages/ControlePlaza.jsx
@@ -6,18 +6,24 @@ const ControlePlaza = () => {
   return (
     <div className="flex">
       <Content titulo={"Controle Plaza"}>
-        <DataTable
-          value={[
-            { id: 1, name: "Plaza A", status: "Active" },
-            { id: 2, name: "Plaza B", status: "Inactive" },
-            { id: 3, name: "Plaza C", status: "Active" },
-          ]}
-          scrollable
-          scrollHeight="400px"
-        >
-          <Column sortable field="id" header="ID" />
-          <Column sortable field="name" header="Name" />
-          <Column sortable field="status" header="Status" />
+        <DataTable scrollable scrollHeight="400px">
+          <Column field="acao" header="Ação" />
+          <Column field="status" header="Status" />
+          <Column field="transferidoSeller" header="Transferido ao seller" />
+          <Column field="cnpj" header="CNPJ" />
+          <Column field="razaoSocial" header="Razão Social" />
+          <Column field="seller" header="Seller" />
+          <Column field="data" header="Data da operação" />
+          <Column field="nota" header="Nota fiscal" />
+          <Column field="taxaPlaza" header="Taxa Plaza" />
+          <Column field="taxaAntecipacao" header="Taxa Antecipação" />
+          <Column field="dataVencimento" header="Data de Vencimento" />
+          <Column field="valordeRepasse" header="Valor de Repasse" />
+          <Column field="repasseTotal" header="Repasse Total do lote" />
+          <Column field="dataDoPagamento" header="Data do Pagamento" />
+          <Column field="numPedido" header="Número do Pedido (Plaza)" />
+          <Column field="dataEntrega" header="Data de Entrega" />
+          <Column field="transportadora" header="Transportadora" />
         </DataTable>
       </Content>
     </div>
