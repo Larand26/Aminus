d52962e125caac6571723ca73c2c99d05685737f
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-06-24 10:38:23 -0300

    feat: instala o Axios para a integração comas transportadoras

---
+++ transportadoras/trackTNT.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+
+const trackTNT = async (data) => {
+  try {
+    for (const nota of data) {
+      const url = `https://radar.tntbrasil.com.br/radar/public/eventoNotaFiscalCliente/json/11984533000171/${nota.NUMERO_NF}.do`;
+      const response = await axios.get(url);
+      if (response.data && response.data.cabecalho) {
+        if (response.data.cabecalho.status === "Entregue") {
+          nota.STATUS = "Pedido Entregue";
+        }
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+trackTNT();
