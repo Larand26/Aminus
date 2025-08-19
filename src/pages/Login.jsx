9490cd936a84b8d9ddccab232350b033d3babbf9
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-08-19 08:17:26 -0300

    style: arruma todo  estilo da página de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,37 @@
+import { useState, useCallback, useEffect } from "react";
+import { InputText } from "primereact/inputtext";
+import { FloatLabel } from "primereact/floatlabel";
+import { Password } from "primereact/password";
+import { Button } from "primereact/button";
+
 const Login = () => {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="home w-full h-screen flex align-items-center justify-content-center">
+      <div className="p-6 flex flex-column align-items-center justify-content-around bg-primary w-11 max-w-30rem h-6 max-h-20rem ">
+        <div className="w-10 flex flex-column align-items-center justify-content-around gap-5">
+          <FloatLabel>
+            <InputText
+              id="user"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+            <label htmlFor="user">Usuário</label>
+          </FloatLabel>
+          <FloatLabel>
+            <Password
+              id="password"
+              feedback={false}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label htmlFor="password">Senha</label>
+          </FloatLabel>
+          <Button label="Entrar" />
+        </div>
+      </div>
     </div>
   );
 };
