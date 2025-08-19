9d88121501d21ceed32a14423d8b579a46dee72f
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-08-19 09:52:25 -0300

    feat: aparece apenas os links para os usuários com a função correspondente

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,80 +4,76 @@ import { useNavigate } from "react-router-dom";
 const Home = () => {
   const navigate = useNavigate();
   const styleLinks = "text-center w-4 mb-2";
+
+  const buttons = [
+    {
+      icon: "pi pi-box",
+      link: "/produtos",
+      label: "Produtos",
+      ids: [1, 2, 3, 4],
+    },
+    {
+      icon: "pi pi-book",
+      link: "/notas",
+      label: "Notas",
+      ids: [1, 2, 3, 4],
+    },
+    {
+      icon: "pi pi-user",
+      link: "/clientes",
+      label: "Clientes",
+      ids: [1, 2, 3],
+    },
+    {
+      icon: "fa fa-cart-shopping",
+      link: "/pedidos",
+      label: "Pedidos",
+      ids: [1, 2, 3, 4],
+    },
+    {
+      icon: "fa fa-box-archive",
+      link: "/reservas",
+      label: "Reservas",
+      ids: [1, 3],
+    },
+    {
+      icon: "fa fa-camera",
+      link: "/fotos",
+      label: "Fotos",
+      ids: [1, 2, 3, 4],
+    },
+    {
+      icon: "fa fa-table",
+      link: "/cadastro-web",
+      label: "Cadastro Web",
+      ids: [1, 3],
+    },
+  ];
+
+  const renderButtons = () => {
+    return buttons
+      .filter((button) =>
+        button.ids.includes(parseInt(localStorage.getItem("userFuncao")))
+      )
+      .map((button) => (
+        <div className={styleLinks} key={button.label}>
+          <Button
+            icon={button.icon}
+            rounded
+            severity="info"
+            aria-label={button.label}
+            onClick={() => navigate(button.link)}
+          />
+          <p className="bebas">{button.label}</p>
+        </div>
+      ));
+  };
+
   return (
     <div className="home w-full h-screen flex align-items-center justify-content-center">
       <div className="p-4 flex flex-column align-items-center justify-content-around bg-primary w-11 max-w-30rem h-6 max-h-20rem ">
         <div className="w-10 flex flex-wrap align-items-center justify-content-around">
-          <div className={styleLinks}>
-            <Button
-              icon="pi pi-box"
-              rounded
-              severity="info"
-              aria-label="Box"
-              onClick={() => navigate("/produtos")}
-            />
-            <p className="bebas">Produtos</p>
-          </div>
-          <div className={styleLinks}>
-            <Button
-              icon="pi pi-book"
-              rounded
-              severity="info"
-              aria-label="Book"
-              onClick={() => navigate("/notas")}
-            />
-            <p className="bebas">Notas</p>
-          </div>
-          <div className={styleLinks}>
-            <Button
-              icon="pi pi-user"
-              rounded
-              severity="info"
-              aria-label="User"
-              onClick={() => navigate("/clientes")}
-            />
-            <p className="bebas">Clientes</p>
-          </div>
-          <div className={styleLinks}>
-            <Button
-              icon="fa fa-cart-shopping"
-              rounded
-              severity="info"
-              aria-label="List"
-              onClick={() => navigate("/pedidos")}
-            />
-            <p className="bebas">Pedidos</p>
-          </div>
-          <div className={styleLinks}>
-            <Button
-              icon="fa fa-box-archive"
-              rounded
-              severity="info"
-              aria-label="List"
-              onClick={() => navigate("/reservas")}
-            />
-            <p className="bebas">Reservas</p>
-          </div>
-          <div className={styleLinks}>
-            <Button
-              icon="fa fa-camera"
-              rounded
-              severity="info"
-              aria-label="List"
-              onClick={() => navigate("/fotos")}
-            />
-            <p className="bebas">Fotos</p>
-          </div>
-          <div className={styleLinks}>
-            <Button
-              icon="fa fa-table"
-              rounded
-              severity="info"
-              aria-label="List"
-              onClick={() => navigate("/cadastro-web")}
-            />
-            <p className="bebas">Cadastro Web</p>
-          </div>
+          {renderButtons()}
         </div>
       </div>
     </div>
