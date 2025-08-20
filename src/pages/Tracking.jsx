be15361520272706173f891b697cb526aadac98a
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-08-20 12:10:23 -0300

    style: arruma tudo no seu lugar

--- src/pages/Tracking.jsx
+++ src/pages/Tracking.jsx
@@ -1,11 +1,18 @@
 import BarraLateral from "../components/BarraLateral";
+import Content from "../components/Content";
+
 import { InputText } from "primereact/inputtext";
 import { FloatLabel } from "primereact/floatlabel";
 import { Dropdown } from "primereact/dropdown";
 import { useState } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 
 import imgTnt from "../assets/img/svg/transportadora/tnt.svg";
 import imgRte from "../assets/img/svg/transportadora/rodonaves.svg";
+
+const styleCards =
+  "bg-gray-100 border-round-lg p-3 h-10rem w-20rem text-center shadow-2 mx-2 font-bold";
 
 const transportadoraOptions = [
   {
@@ -33,9 +40,18 @@ const itemTemplate = (option) => (
 
 const Tracking = () => {
   const [selectedTransportadora, setSelectedTransportadora] = useState(null);
+  const [nota, setNota] = useState("");
+  const [result, setResult] = useState({
+    nome: "",
+    eventos: [],
+    status: "",
+    expectativa: "",
+    nomeDoRecebedor: "",
+    cnpj: "",
+  });
 
   return (
-    <div>
+    <div className="flex">
       <BarraLateral>
         <FloatLabel>
           <InputText id="input-nota" />
@@ -55,6 +71,33 @@ const Tracking = () => {
           <label htmlFor="input-transportadora">Transportadora</label>
         </FloatLabel>
       </BarraLateral>
+      <Content titulo={"Rastreamento"}>
+        <h2>Nome: {result.nome}</h2>
+        <div>
+          <h3>Eventos:</h3>
+          <DataTable value={result.eventos}>
+            <Column field="data" header="Data" />
+            <Column field="descricao" header="Descrição" />
+          </DataTable>
+        </div>
+        <div className="flex w-full justify-content-around mt-4">
+          <div className={styleCards}>
+            Status
+            <br />
+            {result.status}
+          </div>
+          <div className={styleCards}>
+            Expectativa
+            <br />
+            {result.expectativa}
+          </div>
+          <div className={styleCards}>
+            CNPJ
+            <br />
+            {result.cnpj}
+          </div>
+        </div>
+      </Content>
     </div>
   );
 };
