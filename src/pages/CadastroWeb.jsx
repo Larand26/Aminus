3ee512e38ae8ba7188b6d7b6ad6f97199d3fb64f
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-08-22 13:42:52 -0300

    feat: seleciona todos os produtos na tabela

--- src/pages/CadastroWeb.jsx
+++ src/pages/CadastroWeb.jsx
@@ -3,24 +3,34 @@ import { InputText } from "primereact/inputtext";
 import { FloatLabel } from "primereact/floatlabel";
 import { useState, useCallback, useEffect } from "react";
 import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 
 const CadastroWeb = () => {
   const [produtos, setProdutos] = useState([]);
   const [carregando, setCarregando] = useState(false);
   const [referencia, setReferencia] = useState("");
+  const [selectedProdutos, setSelectedProdutos] = useState([]);
 
   const search = () => {
     setCarregando(true);
     window.electronApi?.searchCadastroProdutos(referencia);
     window.electronApi?.onSearchCadastroProdutosResponse((produtos) => {
       setProdutos(produtos);
-      console.log(produtos);
       setCarregando(false);
     });
   };
 
+  // Handler para selecionar todos ao clicar no primeiro radio
+  const onSelectionChange = (e) => {
+    if (e.value.length === 1 && e.value[0] === produtos[0]) {
+      setSelectedProdutos(produtos); // Seleciona todos
+    } else {
+      setSelectedProdutos(e.value);
+    }
+  };
+
   return (
-    <div>
+    <div className="flex">
       <BarraLateral search={search}>
         <FloatLabel>
           <InputText
@@ -31,7 +41,29 @@ const CadastroWeb = () => {
           <label htmlFor="inputNome">Referência</label>
         </FloatLabel>
       </BarraLateral>
-      <div></div>
+      <div>
+        <div></div>
+        <div>
+          <DataTable
+            value={produtos}
+            loading={carregando}
+            scrollable
+            scrollHeight="400px" // Defina a altura desejada
+            selection={selectedProdutos}
+            selectionMode="multiple"
+            onSelectionChange={onSelectionChange}
+          >
+            <Column selectionMode="multiple" headerStyle={{ width: "3rem" }} />
+            <Column field="ID_CODPRODUTO" header="SKU" />
+            <Column field="PROD_DESCRCOMPLETA" header="Descrição" />
+            <Column field="SKU_PRODUTO_PAI" header="Pai" />
+            <Column field="COR_DESCRICAO" header="Cor" />
+            <Column body={"aa"} header="Ativo" />
+            <Column field="PRO_ATIVO_ECOMMERCE" header="Ativo" />
+            <Column field="PRO_INTEGRACAO_ECOMMERCE" header="Integração" />
+          </DataTable>
+        </div>
+      </div>
     </div>
   );
 };
