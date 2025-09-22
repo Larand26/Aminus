52763ce53e25f9f9977afb983fd7273b7296584a
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-09-22 08:47:39 -0300

    style: cria a barra lateral de CadastroWeb

--- src/pages/CadastroWeb.jsx
+++ src/pages/CadastroWeb.jsx
@@ -1,7 +1,23 @@
+import BarraLateral from "../components/BarraLateral";
+import { InputText } from "primereact/inputtext";
+import { FloatLabel } from "primereact/floatlabel";
+
+import { useState, useCallback, useEffect } from "react";
+
 const CadastroWeb = () => {
+  const [referencia, setReferencia] = useState("");
   return (
-    <div>
-      <h1>Cadastro Web</h1>
+    <div className="flex">
+      <BarraLateral>
+        <FloatLabel>
+          <InputText
+            id="inputReferencia"
+            value={referencia}
+            onChange={(e) => setReferencia(e.target.value)}
+          />
+          <label htmlFor="inputReferencia">Referencia</label>
+        </FloatLabel>
+      </BarraLateral>
     </div>
   );
 };
