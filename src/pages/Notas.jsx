da8d7f3cdad804be3364b99d2b9fd1cb25b93618
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-10-15 08:59:09 -0300

    style: arruma a barra latera de notas

--- src/pages/Notas.jsx
+++ src/pages/Notas.jsx
@@ -7,10 +7,17 @@ import InputLabel from "../components/InputLabel";
 import InputDataLabel from "../components/InputDataLabel";
 
 import vendedoresJson from "../assets/json/vendedores.json";
+import ufsJson from "../assets/json/ufs.json";
+import transportadorasJson from "../assets/json/transportadoras.json";
 
 const Notas = () => {
-  const [vendedor, setVendedor] = useState("");
+  // Estados dos inputs
+  const [numNota, setNumNota] = useState("");
+  const [cnpj, setCnpj] = useState("");
   const [data, setData] = useState([null, null]);
+  const [vendedor, setVendedor] = useState("");
+  const [uf, setUf] = useState("");
+  const [transportadora, setTransportadora] = useState("");
 
   const formatarData = (data) => {
     if (!data) return "";
@@ -21,12 +28,39 @@ const Notas = () => {
     <>
       <NavBar />
       <BarraLateral>
+        <InputLabel
+          label="Num Nota"
+          value={numNota}
+          onChange={(e) => setNumNota(e.target.value)}
+        />
+        <InputDataLabel
+          label="Data"
+          value={data}
+          onChange={(value) => setData(value)}
+        />
+        <InputLabel
+          label="Cnpj"
+          value={cnpj}
+          onChange={(e) => setCnpj(e.target.value)}
+        />
         <SelectLabel
           label="Vendedores"
           options={vendedoresJson}
           onChange={(e) => setVendedor(e.target.value)}
+          value={vendedor}
         />
-        <InputDataLabel label="Data" onChange={(value) => setData(value)} />
+        <SelectLabel
+          label="UF"
+          options={ufsJson}
+          onChange={(e) => setUf(e.target.value)}
+          value={uf}
+        />
+        <SelectLabel
+          label="Transportadora"
+          options={transportadorasJson}
+          onChange={(e) => setTransportadora(e.target.value)}
+          value={transportadora}
+        />
       </BarraLateral>
     </>
   );
