11ddbc96264c6afb179ff1f75cb0084e667f7121
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-10-15 11:34:08 -0300

    style: coloca os inputs na barra lateral de pedidos

--- src/pages/Pedidos.jsx
+++ src/pages/Pedidos.jsx
@@ -3,12 +3,36 @@ import { useState } from "react";
 import NavBar from "../components/NavBar";
 import BarraLateral from "../components/BarraLateral";
 
+import SelectLabel from "../components/SelectLabel";
+import InputLabel from "../components/InputLabel";
+import InputDataLabel from "../components/InputDataLabel";
+
 const Pedidos = () => {
+  // Estados dos inputs
+  const [numPedido, setNumPedido] = useState("");
+  const [data, setData] = useState([null, null]);
+  const [cliente, setCliente] = useState("");
+  const [cnpj, setCnpj] = useState("");
+  const [vendedor, setVendedor] = useState("");
   return (
     <>
       <NavBar />
       <div className="main-container">
-        <BarraLateral></BarraLateral>
+        <BarraLateral>
+          <InputLabel
+            label="Num Pedido"
+            value={numPedido}
+            onChange={setNumPedido}
+          />
+          <InputDataLabel label="Data" value={data} onChange={setData} />
+          <InputLabel label="Cliente" value={cliente} onChange={setCliente} />
+          <InputLabel label="CNPJ" value={cnpj} onChange={setCnpj} />
+          <InputLabel
+            label="Vendedor"
+            value={vendedor}
+            onChange={setVendedor}
+          />
+        </BarraLateral>
       </div>
     </>
   );
