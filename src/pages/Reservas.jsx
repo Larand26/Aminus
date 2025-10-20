eda58ec20e65ab74a0cc08f2c9d524e6ebb9f297
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-10-20 10:47:47 -0300

    style: coloca todos os inputs na barra lateral

--- src/pages/Reservas.jsx
+++ src/pages/Reservas.jsx
@@ -2,12 +2,22 @@ import { useState } from "react";
 
 import NavBar from "../components/NavBar";
 import BarraLateral from "../components/BarraLateral";
+import InputLabel from "../components/InputLabel";
+import SelectLabel from "../components/SelectLabel";
+
+import vendedoresJson from "../assets/json/vendedores.json";
 
 const Reservas = () => {
   return (
     <>
       <NavBar />
-      <BarraLateral></BarraLateral>
+      <BarraLateral onSearch={() => {}}>
+        <InputLabel label="Cod Fabricante" />
+        <InputLabel label="Cod Interno" />
+        <InputLabel label="Num Pedido" />
+        <InputLabel label="Nome Pedido" />
+        <SelectLabel label="Vendedor" options={vendedoresJson} />
+      </BarraLateral>
     </>
   );
 };
