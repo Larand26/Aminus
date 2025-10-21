155544d8eeb3c8577f5d3ddb24f69086d426516a
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-10-21 15:44:03 -0300

    feat: muda a pocição apenas arrastando a foto

--- src/components/InputFile.jsx
+++ src/components/InputFile.jsx
@@ -5,6 +5,9 @@ import "../styles/input-file.css";
 const InputFile = (props) => {
   const [files, setFiles] = useState([]);
   const fileInputRef = useRef(null);
+  const dragItem = useRef(null);
+  const dragOverItem = useRef(null);
+  const [dragging, setDragging] = useState(false);
 
   const handleFileChange = (event) => {
     setFiles(Array.from(event.target.files));
@@ -24,6 +27,28 @@ const InputFile = (props) => {
     fileInputRef.current.click();
   };
 
+  const handleDragStart = (e, index) => {
+    dragItem.current = index;
+    setTimeout(() => {
+      setDragging(true);
+    }, 0);
+  };
+
+  const handleDragEnd = () => {
+    setDragging(false);
+    handleSort();
+  };
+
+  const handleSort = () => {
+    if (dragItem.current === null || dragOverItem.current === null) return;
+    const _files = [...files];
+    const draggedItemContent = _files.splice(dragItem.current, 1)[0];
+    _files.splice(dragOverItem.current, 0, draggedItemContent);
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setFiles(_files);
+  };
+
   return (
     <div className="container-input-file">
       <input
@@ -37,7 +62,14 @@ const InputFile = (props) => {
         {files.map((file, index) => (
           <div
             key={index}
-            className="foto-input"
+            className={`foto-input ${
+              dragging && dragItem.current === index ? "dragging" : ""
+            }`}
+            draggable={true}
+            onDragStart={(e) => handleDragStart(e, index)}
+            onDragEnter={() => (dragOverItem.current = index)}
+            onDragEnd={handleDragEnd}
+            onDragOver={(e) => e.preventDefault()}
             onClick={(e) => e.stopPropagation()}
           >
             <button className="remove-file" onClick={() => removeFile(index)}>
