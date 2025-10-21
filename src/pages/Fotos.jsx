073949102f308dd2687691b06a198e6ad99c3892
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-10-21 09:04:25 -0300

    feat: captura os valores dos inputs

--- src/pages/Fotos.jsx
+++ src/pages/Fotos.jsx
@@ -5,14 +5,31 @@ import BarraLateral from "../components/BarraLateral";
 import InputLabel from "../components/InputLabel";
 
 const Fotos = () => {
+  // Estados dos inputs
+  const [codFabricante, setCodFabricante] = useState("");
+  const [codInterno, setCodInterno] = useState("");
+  const [codCor, setCodCor] = useState("");
+
   return (
     <>
       <NavBar />
       <div className="main-container">
         <BarraLateral onSearch={() => {}}>
-          <InputLabel label="Cod Fabricante" value="" onChange={() => {}} />
-          <InputLabel label="Cod Interno" value="" onChange={() => {}} />
-          <InputLabel label="Cod Cor" value="" onChange={() => {}} />
+          <InputLabel
+            label="Cod Fabricante"
+            value={codFabricante}
+            onChange={(e) => setCodFabricante(e.target.value)}
+          />
+          <InputLabel
+            label="Cod Interno"
+            value={codInterno}
+            onChange={(e) => setCodInterno(e.target.value)}
+          />
+          <InputLabel
+            label="Cod Cor"
+            value={codCor}
+            onChange={(e) => setCodCor(e.target.value)}
+          />
         </BarraLateral>
       </div>
     </>
