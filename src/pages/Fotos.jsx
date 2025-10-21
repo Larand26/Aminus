c5c35d102f027e88643c914201282400d3494b29
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-10-21 09:01:11 -0300

    style: cria a barra lateral de fotos

--- src/pages/Fotos.jsx
+++ src/pages/Fotos.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
+import NavBar from "../components/NavBar";
+import BarraLateral from "../components/BarraLateral";
+import InputLabel from "../components/InputLabel";
+
 const Fotos = () => {
   return (
-    <div>
-      <h1>Fotos</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="main-container">
+        <BarraLateral onSearch={() => {}}>
+          <InputLabel label="Cod Fabricante" value="" onChange={() => {}} />
+          <InputLabel label="Cod Interno" value="" onChange={() => {}} />
+          <InputLabel label="Cod Cor" value="" onChange={() => {}} />
+        </BarraLateral>
+      </div>
+    </>
   );
 };
 
