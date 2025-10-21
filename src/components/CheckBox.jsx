8a179ef325c545c50eecdf06de7133837e49b210
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-10-21 09:36:42 -0300

    style: cria a checkbox customizado

---
+++ src/components/CheckBox.jsx
@@ -0,0 +1,13 @@
+import "../styles/checkbox.css";
+
+const CheckBox = ({ id, checked, onChange }) => {
+  return (
+    <div className="custom-checkbox">
+      <input type="checkbox" id={id} checked={checked} onChange={onChange} />
+      {/* A label é usada para criar o visual do checkbox */}
+      <label htmlFor={id}></label>
+    </div>
+  );
+};
+
+export default CheckBox;
--- src/pages/Fotos.jsx
+++ src/pages/Fotos.jsx
@@ -3,12 +3,15 @@ import { useState } from "react";
 import NavBar from "../components/NavBar";
 import BarraLateral from "../components/BarraLateral";
 import InputLabel from "../components/InputLabel";
+import CheckBox from "../components/CheckBox";
 
 const Fotos = () => {
   // Estados dos inputs
   const [codFabricante, setCodFabricante] = useState("");
   const [codInterno, setCodInterno] = useState("");
   const [codCor, setCodCor] = useState("");
+
+  const [c, setC] = useState(false);
 
   return (
     <>
@@ -31,6 +34,16 @@ const Fotos = () => {
             onChange={(e) => setCodCor(e.target.value)}
           />
         </BarraLateral>
+        <div className="content">
+          <div className="content-title">
+            <h1>Fotos</h1>
+          </div>
+          <div className="container-fotos">
+            <div className="navBar-fotos">
+              <CheckBox id="foto1" checked={c} onChange={() => setC(!c)} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
