4d05b0129529da1e1d73e7aef3bdc107dd278a87
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-10-21 15:28:22 -0300

    feat: coloca as imagens no input

--- src/components/InputFile.jsx
+++ src/components/InputFile.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import "../styles/input-file.css";
 
 const InputFile = (props) => {
   const [files, setFiles] = useState([]);
+  const fileInputRef = useRef(null);
 
   const handleFileChange = (event) => {
     setFiles(Array.from(event.target.files));
@@ -19,12 +20,26 @@ const InputFile = (props) => {
     );
   };
 
+  const openFileDialog = () => {
+    fileInputRef.current.click();
+  };
+
   return (
     <div className="container-input-file">
-      <input type="file" onChange={handleFileChange} multiple />
-      <div className="fotos-input">
+      <input
+        type="file"
+        onChange={handleFileChange}
+        multiple
+        ref={fileInputRef}
+        style={{ display: "none" }}
+      />
+      <div className="fotos-input" onClick={openFileDialog}>
         {files.map((file, index) => (
-          <div key={index} className="foto-input">
+          <div
+            key={index}
+            className="foto-input"
+            onClick={(e) => e.stopPropagation()}
+          >
             <button className="remove-file" onClick={() => removeFile(index)}>
               <i className="fas fa-trash"></i>
             </button>
