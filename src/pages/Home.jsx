4e1bcacca9a27915ad648a9c93539721fc3571a9
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-10-30 10:00:15 -0300

    feat: não aparece as páginas restritas para o usuário

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,22 +10,29 @@ import "../styles/home.css";
 
 const Home = () => {
   const navigate = useNavigate();
+
+  // Usuário
+  const idFuncao = JSON.parse(localStorage.getItem("ID_FUNCAO_USUARIO"));
+
   return (
     <div>
       <NavBar page="home" />
       <div className="home-container">
-        {pages.map((page) => (
-          <Card
-            className="hover-card"
-            key={page.page}
-            icon={page.icon}
-            onClick={() => navigate(page.path)}
-          >
-            <div className="card-container">
-              <p>{page.title}</p>
-            </div>
-          </Card>
-        ))}
+        {pages.map((page) => {
+          if (page.restrition.includes(idFuncao)) return null;
+          return (
+            <Card
+              className="hover-card"
+              key={page.page}
+              icon={page.icon}
+              onClick={() => navigate(page.path)}
+            >
+              <div className="card-container">
+                <p>{page.title}</p>
+              </div>
+            </Card>
+          );
+        })}
       </div>
     </div>
   );
