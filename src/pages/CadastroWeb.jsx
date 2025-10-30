5a29899abb4adb6ad3aa9c6397c3f0c5026ec65f
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-10-30 07:48:23 -0300

    style: finaliza a navbar e a barra lateral de cadastro web

--- src/pages/CadastroWeb.jsx
+++ src/pages/CadastroWeb.jsx
@@ -1,4 +1,18 @@
+import NavBar from "../components/NavBar";
+import BarraLateral from "../components/BarraLateral";
+import InputLabel from "../components/InputLabel";
+
 const CadastroWeb = () => {
-  return <div>CadastroWeb</div>;
+  return (
+    <>
+      <NavBar />
+      <div className="main-container">
+        <BarraLateral>
+          <InputLabel label="Cod Fabricante" />
+          <InputLabel label="Cod Interno" />
+        </BarraLateral>
+      </div>
+    </>
+  );
 };
 export default CadastroWeb;
