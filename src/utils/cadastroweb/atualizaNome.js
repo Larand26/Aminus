f78c245a6780af996a385bdff23c44f2b1f14012
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-11-06 09:35:21 -0300

    add: adiciona o código de fabricante ao nome atualizado

--- src/utils/cadastroweb/atualizaNome.js
+++ src/utils/cadastroweb/atualizaNome.js
@@ -77,6 +77,6 @@ const atualizaNome = (itemSelecionado) => {
       break;
   }
 
-  return nomeAtualizado;
+  return nomeAtualizado + " - " + itemSelecionado.codFabricante;
 };
 export default atualizaNome;
