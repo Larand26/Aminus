7bb8660ff876042412143a6e4b644c03d6d1c2fc
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-12-10 08:27:00 -0300

    feat: arruma o json de respostas do Gemini

--- src/components/Gemini.jsx
+++ src/components/Gemini.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import InputButton from "../components/InputButton";
+import Toast from "../components/Toast";
 
 import pegaRespostaGemini from "../utils/pegaRespostaGemini";
 
@@ -8,29 +9,51 @@ import "../styles/gemini.css";
 
 const Gemini = () => {
   const [pergunta, setPergunta] = useState("");
+  const [respostas, setRespostas] = useState([]);
+
+  const [toastInfo, setToastInfo] = useState(null);
 
   const handleEnviarPergunta = async () => {
     if (pergunta.trim() === "") return;
+
+    setRespostas([...respostas, { conteudo: pergunta, user: "usuário" }]);
+    setPergunta("");
+
     const resposta = await pegaRespostaGemini(pergunta);
+
+    if (!resposta.success) {
+      setToastInfo({
+        tipo: "erro",
+        mensagem: resposta.mensagem || "Erro ao obter resposta do Gemini.",
+      });
+      return;
+    }
+    setRespostas((prevRespostas) => [
+      ...prevRespostas,
+      { conteudo: resposta.conteudo, user: "gemini" },
+    ]);
+
     console.log(resposta);
   };
 
+  useEffect(() => {
+    if (toastInfo) {
+      const timer = setTimeout(() => {
+        setToastInfo(null);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [toastInfo]);
+
   return (
     <div className="gemini-container" id="gemini">
+      {toastInfo && (
+        <Toast tipo={toastInfo.tipo} mensagem={toastInfo.mensagem} />
+      )}
       <div className="respostas">
         <div className="mensagem-gemini">
           <p>Olá! Como posso ajudar você hoje?</p>
-        </div>
-        <div className="mensagem-usuario">
-          <p>Me faz o texto de Lorem</p>
-        </div>
-        <div className="mensagem-gemini">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae,
-            eligendi. Quaerat pariatur tempore eos incidunt quis delectus
-            voluptas, excepturi iusto, ducimus architecto totam omnis corporis
-            suscipit nam, quae consequuntur inventore.
-          </p>
         </div>
       </div>
       <div className="input-container-gemini ">
