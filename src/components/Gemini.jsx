2a3550bf9bcc58ced926909fa9fdb3024b447376
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-12-10 07:27:38 -0300

    style: arruma o layout do gemini

--- src/components/Gemini.jsx
+++ src/components/Gemini.jsx
@@ -3,7 +3,14 @@ import "../styles/gemini.css";
 const Gemini = () => {
   return (
     <div className="gemini-container" id="gemini">
-      <h1>Gemini</h1>
+      <div className="respostas"></div>
+      <div className="input-container-gemini ">
+        <input
+          type="text"
+          className="gemini-input"
+          placeholder="Digite sua pergunta"
+        />
+      </div>
     </div>
   );
 };
