7b33ad2f19c92c81a889714a418465eabbd280f1
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-12-10 07:46:19 -0300

    style: arruma o layout das respostas e input do Gemini

--- src/components/Gemini.jsx
+++ src/components/Gemini.jsx
@@ -1,14 +1,30 @@
+import InputButton from "../components/InputButton";
+
 import "../styles/gemini.css";
 
 const Gemini = () => {
   return (
     <div className="gemini-container" id="gemini">
-      <div className="respostas"></div>
+      <div className="respostas">
+        <div className="mensagem-gemini">
+          <p>Olá! Como posso ajudar você hoje?</p>
+        </div>
+        <div className="mensagem-usuario">
+          <p>Me faz o texto de Lorem</p>
+        </div>
+        <div className="mensagem-gemini">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae,
+            eligendi. Quaerat pariatur tempore eos incidunt quis delectus
+            voluptas, excepturi iusto, ducimus architecto totam omnis corporis
+            suscipit nam, quae consequuntur inventore.
+          </p>
+        </div>
+      </div>
       <div className="input-container-gemini ">
-        <input
-          type="text"
-          className="gemini-input"
-          placeholder="Digite sua pergunta"
+        <InputButton
+          placeholder="Digite sua mensagem..."
+          icon="fa fa-paper-plane"
         />
       </div>
     </div>
