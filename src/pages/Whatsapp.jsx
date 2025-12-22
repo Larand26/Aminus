c082c159c6798e9723b997b173e94b38766b251a
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-12-22 13:12:09 -0300

    style: ajusta os inputs da barra lateral

--- src/pages/Whatsapp.jsx
+++ src/pages/Whatsapp.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
+import NavBar from "../components/NavBar";
+import BarraLateral from "../components/BarraLateral";
+import InputLabel from "../components/InputLabel";
+import SelectLabel from "../components/SelectLabel";
+
+import vendedoresJson from "../assets/json/vendedores.json";
+
 const Whatsapp = () => {
+  // Estados dos inputs
+  const [numero, setNumero] = useState("");
+  const [mensagem, setMensagem] = useState("");
+  const [vendedor, setVendedor] = useState("");
+
+  // Pesquisa
+  const handleSearch = () => {};
+
   return (
     <>
-      <></>
+      <NavBar />
+      <BarraLateral onSearch={handleSearch}>
+        <InputLabel label="Número" value={numero} onChange={setNumero} />
+        <InputLabel label="Cliente" value={mensagem} onChange={setMensagem} />
+        <SelectLabel
+          label="Vendedor"
+          options={vendedoresJson}
+          value={vendedor}
+          onChange={setVendedor}
+        />
+      </BarraLateral>
     </>
   );
 };
