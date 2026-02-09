3b22a39f62ca3a033595c2ce40f0d3521f035820
Victor Laranjeira <vbl09012003vbl@gmail.com>
2026-02-09 14:43:03 -0300

    feat: adiciona arquivos no input

--- src/components/InputFile.jsx
+++ src/components/InputFile.jsx
@@ -1,14 +1,26 @@
-import { useState, useRef, useEffect } from "react";
+import { useRef } from "react";
 
 import "../styles/inputs/input-file.css";
 
 const InputFile = (props) => {
+  const inputRef = useRef();
+
+  const handleButtonClick = () => {
+    inputRef.current.click();
+  };
+
   return (
-    <>
-      <div className="input-file-container">
-        <input type="file" name="inputfile" id={props.id} />
-      </div>
-    </>
+    <div className="input-file-container" onClick={handleButtonClick}>
+      <input
+        type="file"
+        name="inputfile"
+        id={props.id}
+        ref={inputRef}
+        style={{ display: "none" }}
+        onChange={props.onChange}
+        multiple
+      />
+    </div>
   );
 };
 export default InputFile;
