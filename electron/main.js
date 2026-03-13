c23c7eafa342e8430ccfb735e3c76a9f5320c1b2
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-03-13 10:56:21 -0300

    feat: abre a tela do electron

--- electron/main.js
+++ electron/main.js
@@ -1,7 +1,11 @@
 import { app, BrowserWindow, ipcMain } from "electron";
 import path from "path";
+import { fileURLToPath } from "url";
 
-import ElectronConfig from "./src/config/ElectronConfig";
+import ElectronConfig from "./src/config/ElectronConfig.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -19,8 +23,22 @@ const createWindow = () => {
   if (ElectronConfig.isDev) {
     win.loadURL("http://localhost:5173");
   } else {
-    win.loadFile(path.join(__dirname, "build/index.html"));
+    win.loadFile(path.join(__dirname, "../build/index.html"));
   }
 };
 
-createWindow();
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
